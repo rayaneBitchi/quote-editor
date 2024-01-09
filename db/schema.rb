3e6d39e2b3357f5b8e3d34b878244ce249c855b5
Rayane Bitchi <rayanebitchi@icloud.com>
2024-01-09 04:38:12 -0500

    device gem added

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_08_212716) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_09_093256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "quotes", force: :cascade do |t|
     t.string "name", null: false
