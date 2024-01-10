7f2a52730e6c4745c5a93359895e2edd3abbe1af
Rayane Bitchi <rayanebitchi@icloud.com>
2024-01-10 05:47:01 -0500

    line item design done

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :quotes do
-    resources :line_item_dates, expect: %i[index show]
+    resources :line_item_dates, expect: %i[index show] do
+      resources :line_items, expect: %i[index show]
+    end
   end
 end
